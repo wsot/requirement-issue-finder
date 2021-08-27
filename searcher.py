a6f325fda7a22e7b0b2332bd123cb090f5107d28
Simeon J Morgan <smorgan@digitalfeed.net>
2021-08-27 19:23:24 +1000

    binary search thing works now

--- searcher.py
+++ searcher.py
@@ -8,15 +8,24 @@ class EndNodeReached(Exception):
 
 
 class BinarySearcher:
-    def __init__(self, initial_tree):
+    def __init__(self, initial_tree, initial_split_children=False):
         self._initial_tree = initial_tree
         self._traversal = [[initial_tree]]
-        # self._traversal = [[initial_tree, initial_tree[: len(initial_tree // 2)]]]
+        self._initial_split_children = initial_split_children
 
     def passed(self):
+        # if there is no parent list, we're done
+        if not len(self._traversal) > 0:
+            return None
+
+            # if the parent list is only one item, skip past it as it has already been traversed downward
+        if not len(self._traversal[-1]) > 1:
+            self._traversal.pop()
+            return self.passed()
+
         # if this is the first half, go to the second half
         if self._traversal[-1][-2][0] == self._traversal[-1][-1][0]:
-            self._traversal[-1][-1] = self._traversal[-1][-2][len(self._traversal[-1]) :]
+            self._traversal[-1][-1] = self._traversal[-1][-2][len(self._traversal[-1][-2]) // 2 :]
             return self._traversal[-1][-1]
 
         # otherwise this must already be the second half of the current subset
@@ -27,14 +36,6 @@ class BinarySearcher:
 
         # go up a level in the current subset and try again
         self._traversal[-1].pop()
-
-        # if there isn't another level inside the current subset, move to the parent subset then try again
-        if not len(self._traversal[-1]) > 1:
-            self._traversal.pop()
-            if not len(self._traversal) > 1:
-                # Ok, that's the end of the pile
-                return
-            self._traversal.pop()
         return self.passed()
 
     def failed(self):
@@ -45,24 +46,12 @@ class BinarySearcher:
         # if there is only a single item, see if it has children. If it has children, bisect them
         children = self._traversal[-1][-1][0].get_children()
         if children:
-        #     self._traversal.append([children, children[: len(children) // 2]])
-            self._traversal.append([children])
+            if self._initial_split_children:
+                # Assume that the parent is equivalent to the full list of children, so split the children immediately
+                self._traversal.append([children, children[: len(children) // 2]])
+            else:
+                # Assume that the parent and full list of children are distinct,
+                #  so each should be independently represented when traversing the tree
+                self._traversal.append([children])
             return self._traversal[-1][-1]
         raise EndNodeReached(self._traversal[-1][-1][0])
-
-    # if first item of current bottom level == first item of parent level, then you're doing the first half
-
-    # start with half of the top level of the unpinned. If that passes, get the next half of that level of the tree.
-    # if it fails, test with half of that top level.
-    # If we're down to one in the top level, go to half the children of that. IF it passes, go to the next half. If it fails, split the current half.
-    # if the current half is 'one', then see if it has children. If it has children, test pinning them.
-
-    # test with none pinned; if fails, pin half and all descendents of that half; if p
-
-    # if something fails when unpinned, but passes when pinned, that thing being pinned is contributing
-    # then try to isolate the smallest group that can be pinned
-
-    # start with nothing pinned
-
-    # failure can be because top level version changes, or because children change - need to be able to differentiate
-    # if fully pinned passes, unpin half of tree all the way down. If that passes, unpin half that tree all the way down. If it fails, pin the other half.
--- test_binary_search.py
+++ test_binary_search.py
@@ -4,6 +4,7 @@ import pytest
 
 import typing as t
 
+
 class ThingToBeSearched:
     def __init__(self, name: str, children: t.Optional[list["ThingToBeSearched"]] = None):
         self.name = name
@@ -19,7 +20,128 @@ class ThingToBeSearched:
 
 
 class TestBinarySearcher:
-    def test_traverses(self):
+    def test_single_chain(self):
+        things_to_be_searched = [ThingToBeSearched("A", [ThingToBeSearched("A.A", [ThingToBeSearched("A.A.A")])])]
+
+        binary_searcher = BinarySearcher(things_to_be_searched)
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A.A"]
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A.A.A"]
+        with pytest.raises(EndNodeReached) as excinfo:
+            subset = binary_searcher.failed()
+        excinfo.value.requirement.name = "A.A.A"
+        subset = binary_searcher.passed()
+        assert subset == None
+
+    def test_single_layer_pair(self):
+        things_to_be_searched = [ThingToBeSearched("A"), ThingToBeSearched("B")]
+
+        binary_searcher = BinarySearcher(things_to_be_searched)
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A"]
+        with pytest.raises(EndNodeReached) as excinfo:
+            subset = binary_searcher.failed()
+        excinfo.value.requirement.name = "A"
+        subset = binary_searcher.passed()
+        assert [r.name for r in subset] == ["B"]
+        with pytest.raises(EndNodeReached) as excinfo:
+            subset = binary_searcher.failed()
+        excinfo.value.requirement.name = "B"
+        subset = binary_searcher.passed()
+        assert subset is None
+
+    def test_uneven_number(self):
+        things_to_be_searched = [ThingToBeSearched("A"), ThingToBeSearched("B"), ThingToBeSearched("C")]
+
+        binary_searcher = BinarySearcher(things_to_be_searched)
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A"]
+        with pytest.raises(EndNodeReached) as excinfo:
+            subset = binary_searcher.failed()
+        excinfo.value.requirement.name = "A"
+        subset = binary_searcher.passed()
+        assert [r.name for r in subset] == ["B", "C"]
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["B"]
+        with pytest.raises(EndNodeReached) as excinfo:
+            subset = binary_searcher.failed()
+        excinfo.value.requirement.name = "B"
+        subset = binary_searcher.passed()
+        assert [r.name for r in subset] == ["C"]
+        subset = binary_searcher.passed()
+        assert subset is None
+
+    def test_multiple_under_single_no_initial_split_children(self):
+        things_to_be_searched = [ThingToBeSearched("A", [ThingToBeSearched("A.A"), ThingToBeSearched("A.B")])]
+
+        binary_searcher = BinarySearcher(things_to_be_searched, initial_split_children=False)
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A.A", "A.B"]
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A.A"]
+        with pytest.raises(EndNodeReached) as excinfo:
+            subset = binary_searcher.failed()
+        excinfo.value.requirement.name = "A.A.A"
+        subset = binary_searcher.passed()
+        assert [r.name for r in subset] == ["A.B"]
+        with pytest.raises(EndNodeReached) as excinfo:
+            subset = binary_searcher.failed()
+        excinfo.value.requirement.name = "A.B"
+        subset = binary_searcher.passed()
+        assert subset is None
+
+    def test_multiple_under_single_initial_split_children(self):
+        things_to_be_searched = [ThingToBeSearched("A", [ThingToBeSearched("A.A"), ThingToBeSearched("A.B")])]
+
+        binary_searcher = BinarySearcher(things_to_be_searched, initial_split_children=True)
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A.A"]
+        with pytest.raises(EndNodeReached) as excinfo:
+            subset = binary_searcher.failed()
+        excinfo.value.requirement.name = "A.A"
+        subset = binary_searcher.passed()
+        assert [r.name for r in subset] == ["A.B"]
+        with pytest.raises(EndNodeReached) as excinfo:
+            subset = binary_searcher.failed()
+        excinfo.value.requirement.name = "A.B"
+        subset = binary_searcher.passed()
+        assert subset is None
+
+    def test_unequal_depth(self):
+        things_to_be_searched = [
+            ThingToBeSearched(
+                "A",
+                [
+                    ThingToBeSearched("A.A"),
+                    ThingToBeSearched("A.B", [ThingToBeSearched("A.B.A"), ThingToBeSearched("A.B.B")]),
+                ],
+            )
+        ]
+
+        binary_searcher = BinarySearcher(things_to_be_searched)
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A.A", "A.B"]
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A.A"]
+        with pytest.raises(EndNodeReached) as excinfo:
+            subset = binary_searcher.failed()
+        excinfo.value.requirement.name = "A.A"
+        subset = binary_searcher.passed()
+        assert [r.name for r in subset] == ["A.B"]
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A.B.A", "A.B.B"]
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A.B.A"]
+        subset = binary_searcher.passed()
+        assert [r.name for r in subset] == ["A.B.B"]
+        with pytest.raises(EndNodeReached) as excinfo:
+            subset = binary_searcher.failed()
+        excinfo.value.requirement.name = "A.B.B"
+        subset = binary_searcher.passed()
+        assert subset is None
+
+    def test_traverses_without_autosplit_children(self):
         things_to_be_searched = [
             ThingToBeSearched(
                 "A",
@@ -74,3 +196,19 @@ class TestBinarySearcher:
         excinfo.value.requirement.name = "A.C"
         subset = binary_searcher.passed()
         assert [r.name for r in subset] == ["A.D"]
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A.D.A", "A.D.B"]
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A.D.A"]
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A.D.A.A", "A.D.A.B"]
+        subset = binary_searcher.failed()
+        assert [r.name for r in subset] == ["A.D.A.A"]
+        with pytest.raises(EndNodeReached) as excinfo:
+            subset = binary_searcher.failed()
+        excinfo.value.requirement.name = "A.D.A.A"
+        subset = binary_searcher.passed()
+        with pytest.raises(EndNodeReached) as excinfo:
+            subset = binary_searcher.failed()
+        excinfo.value.requirement.name = "A.D.A.B"
+        subset = binary_searcher.passed()
